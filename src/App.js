a7ba9b8e29064e7fcc9755fe29d6a4a6b6095bab
Gayan65 <gayan65728@yahoo.com>
2025-04-16 18:20:39 +0300

    nav bar component add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
