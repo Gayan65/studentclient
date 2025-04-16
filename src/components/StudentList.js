291e0a431b25ce95d77b748803c33666df464edb
Gayan65 <gayan65728@yahoo.com>
2025-04-16 19:01:27 +0300

    console log remove

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -21,7 +21,6 @@ const StudentList = () => {
         };
 
         fetchAllStudents();
-        console.log(students);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
